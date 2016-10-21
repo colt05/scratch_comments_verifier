680b6f9e7863ac295b2ccb979ef24ee2a76b0006
colt05-usr <colt05-usr@users.noreply.github.com>
2016-10-21 17:27:32 -0400

    Update index.php

--- web/index.php
+++ web/index.php
@@ -2,10 +2,19 @@
 
 require('../vendor/autoload.php');
 
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
 $app = new Silex\Application();
+
 
 $app->get('/', function() use($app) {
 return "9 + 10"; 
+});
+
+$app->post('/test', function (Request $request) {
+    $message = $request->get('test');
+    return test;
 });
 
 $app->run();
