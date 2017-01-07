d0dfd4cb60a950d2ba0932e18f70d5e6f5a07ea9
colt05-usr <colt05-usr@users.noreply.github.com>
2017-01-07 08:20:49 -0500

    Finally fix #1!! (eighth attempt)

--- web/index.php
+++ web/index.php
@@ -39,7 +39,7 @@ $app->post('/userCode', function (Request $request) {
         $userIndexOfWeirdString = strpos($user, '" id="comment-user" data-comment-user="'); //Offset
         $user2222 = substr($user, $userIndexOfWeirdString);
         $user3333 = substr($user, strlen('" id="comment-user" data-comment-user="'));
-        $user4444 = substr($user3333, strlen('user="') + 1);
+        $user4444 = substr($user3333, strlen('user="'));
         $user = $user4444;
         echo $user;
         echo "=======";
