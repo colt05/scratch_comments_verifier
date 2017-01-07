48a218f53570d744d4d7e0b2f912b1fa1f9fd3ad
colt05-usr <colt05-usr@users.noreply.github.com>
2017-01-07 09:37:10 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
 var express = require('express');
 var httppost = require("http-post");
+var sha1 = require('sha1');
 var app = express();
- 
+
+function code(ip) {
+  var a = Math.floor(Date.now() / 1800);
+  var timeStampString = a.toString();
+  var b = timeStampString.concat(navigator.userAgent);
+  var ip = myIP();
+  var c = b.concat(ip);
+}
+
 app.get('/', function (req, res) {
   res.send('Listening on port '.concat(process.env.PORT));
-})
+});
  
+app.get("/getCode", function(req, res) {
+ var a = Math.floor(Date.now() / 1800);
+  var timeStampString = a.toString();
+  var b = timeStampString.concat(req.headers['user-agent']); //fixed for express
+  var ip = req.ip; //fixed for express 
+  var c = b.concat(ip);
+  var jsonObj = {};
+  jsonObj.codeDec = sha1(c); //fixed to use node
+  res.end(JSON.stringify(jsonObj));
+});
 function ab2str(buf) {
   return String.fromCharCode.apply(null, new Uint8Array(buf));
 }
